2119d201349bfd3a240c290fa74986cb06ac2487
Ferhat Ziba <fero.ziba@gmail.com>
2016-06-10 18:18:57 +0200

    add pattern matching

--- lib/sentino.rb
+++ lib/sentino.rb
@@ -2,4 +2,5 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 module Sentino
   autoload :Word, 'sentino/word'
+  autoload :Pattern, 'sentino/pattern'
 end
--- test/test_sentino.rb
+++ test/test_sentino.rb
@@ -9,12 +9,28 @@ class TestSentino < Test::Unit::TestCase
     assert_equal 1.0, sentino.match(target_1)
   end
 
-  should "should give 75 % identical Words" do 
+  should "should give 75 % identical Words" do
     reference = "Der erste String Test"
     target_1 = reference.gsub("erste", "zweite")
     sentino = Sentino::Word.new(reference)
     assert_equal 0.75, sentino.match(target_1)
-
   end
+
+  should "give a higher value for baby then for tee" do
+    reference = "Babyprodukte / Babynahrung / Kekse & Tee / Babynahrung ab 4. Monat"
+    tee = "Geträn­ke Kaffee & Tee Tee Grüner & weißer Tee Weißer Tee"
+    baby = "Baby Babynahrung Kindersnacks Babykekse"
+    sentino = Sentino::Pattern.new(reference)
+    assert_true sentino.match(tee) < sentino.match(baby)
+  end
+
+  should "give a higher value for suger then for obst" do
+    reference = "Nahrungsmittel / Zucker & Süßstoff / Süßstoff / Süßstoff"
+    obst = "Obst & Gemüse Gemüse Zwiebeln & Knoblauch Zwiebeln"
+    suger = "Lebens­mittel Mehl Stärke & Co. Zucker & Süße Süßstoff"
+    sentino = Sentino::Pattern.new(reference)
+    assert_true sentino.match(obst) < sentino.match(suger)
+  end
+
 
 end
